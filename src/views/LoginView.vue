6689c19c98b79129c8d40924a4ba7806e8a0ed78
valentinoghitti <valentinoghitti00@hotmail.com>
2024-07-25 20:28:28 -0300

    fix(PC-13): resolve registration bug & add comments

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,15 +6,17 @@
           <v-window v-model="step" class="fill-height">
             <v-window-item :value="1">
               <v-row no-gutters class="fill-height ma-0 pa-0">
+                <!-- Columna para el formulario de inicio de sesión -->
                 <v-col cols="12" md="6" class="pa-0 ma-0">
                   <v-card-text class="mt-12">
-                    <h4 class="text-center">Login in to Your Account</h4>
-                    <h6 class="text-center grey--text">
-                      Log in to your account so you can continue building <br>
-                      and editing your onboarding flows
-                    </h6>
+                    <h2 class="text-center">Login in to Your Account</h2>
+                    <h4 class="text-center grey--text">
+                      Log in to your account to continue managing and <br>
+                      updating your pet care routines.
+                    </h4>
                     <v-row align="center" justify="center">
                       <v-col cols="12" sm="8">
+                        <!-- Formulario de inicio de sesión -->
                         <v-form ref="form" v-model="valid">
                           <v-text-field
                             v-model="email"
@@ -48,9 +50,11 @@
                               <span class="caption blue--text">Forgot password</span>
                             </v-col>
                           </v-row>
+                          <!-- Botón de inicio de sesión -->
                           <v-btn color="blue" dark block tile @click="handleLogin">
                             Log in
                           </v-btn>
+                          <!-- Mensaje de error de inicio de sesión -->
                           <v-fade-transition>
                             <v-col>
                               <v-alert
@@ -62,23 +66,24 @@
                                 {{ loginError }}
                               </v-alert>
                             </v-col>
-
                           </v-fade-transition>
                         </v-form>
                       </v-col>
                     </v-row>
                   </v-card-text>
                 </v-col>
+                <!-- Columna para el mensaje de registro -->
                 <v-col cols="12" md="6" class="blue rounded-bl-xl fill-height pa-0 ma-0">
                   <div style="text-align: center; padding: 210px 0;">
                     <v-card-text class="white--text">
-                      <h3 class="text-center">Don't Have an Account Yet?</h3>
-                      <h6 class="text-center">
-                        Let's get you all set up so you can start creating your first<br>
-                        onboarding experience
-                      </h6>
+                      <h2 class="text-center">Don't Have an Account Yet?</h2><br>
+                      <h4 class="text-center">
+                        Let’s set it up so you can start caring for your pets.<br>
+                        Join us!
+                      </h4>
                     </v-card-text>
                     <div class="text-center">
+                      <!-- Botón para ir al registro -->
                       <v-btn tile outlined dark @click="step++">
                         SIGN UP
                       </v-btn>
@@ -87,17 +92,17 @@
                 </v-col>
               </v-row>
             </v-window-item>
-
+            <!-- Segundo paso de la ventana (Registro) -->
             <v-window-item :value="2">
               <v-row no-gutters class="fill-height ma-0 pa-0">
                 <v-col cols="12" md="6" class="blue rounded-br-xl fill-height pa-0 ma-0">
                   <div style="text-align: center; padding: 210px 0;">
                     <v-card-text class="white--text">
-                      <h3 class="text-center">Already Signed up?</h3>
-                      <h6 class="text-center">
-                        Log in to your account so you can continue building and<br>
-                        editing your onboarding flows
-                      </h6>
+                      <h2 class="text-center">Log in to manage your PetCare.</h2><br>
+                      <h4 class="text-center">
+                        Log in to your account so you can continue <br>
+                        setting up and managing your pet care routines.
+                      </h4>
                     </v-card-text>
                     <div class="text-center">
                       <v-btn tile outlined dark @click="step--">
@@ -106,19 +111,21 @@
                     </div>
                   </div>
                 </v-col>
-
+                <!-- Columna para el formulario de registro -->
                 <v-col cols="12" md="6" class="pa-0 ma-0">
                   <v-card-text class="mt-12">
-                    <h4 class="text-center">Sign Up for an Account</h4>
-                    <h6 class="text-center grey--text">
-                      Let's get you all set up so you can start creating your <br>
-                      first onboarding experience
-                    </h6>
+                    <h2 class="text-center">Sign Up for an Account</h2>
+                    <h4 class="text-center grey--text">
+                      Let’s get you set up so you can start managing <br>
+                      your pet care routines.
+                    </h4>
                     <v-row align="center" justify="center">
                       <v-col cols="12" sm="8">
                         <v-row>
+                          <!-- Campos para el nombre y apellido -->
                           <v-col cols="12" sm="6">
                             <v-text-field
+                              v-model="firstName"
                               label="First Name"
                               outlined
                               dense
@@ -129,6 +136,7 @@
                           </v-col>
                           <v-col cols="12" sm="6">
                             <v-text-field
+                              v-model="lastName"
                               label="Last Name"
                               outlined
                               dense
@@ -139,6 +147,7 @@
                           </v-col>
                         </v-row>
                         <v-text-field
+                          v-model="email"
                           label="Email"
                           outlined
                           dense
@@ -146,6 +155,7 @@
                           autocomplete="false"
                         />
                         <v-text-field
+                          v-model="password"
                           label="Password"
                           outlined
                           dense
@@ -165,13 +175,13 @@
                             <span class="caption blue--text ml-n4">Terms & Conditions</span>
                           </v-col>
                         </v-row>
+                        <!-- Botón para registrar -->
                         <v-btn color="blue" @click="handleRegister" dark block tile>
                           Sign up
                         </v-btn>
                       </v-col>
                     </v-row>
                   </v-card-text>
-
                 </v-col>
               </v-row>
             </v-window-item>
@@ -187,11 +197,13 @@ import { mapActions } from 'vuex';
 
 export default {
   data: () => ({
-    step: 1,
+    step: 1, // Controla el paso actual de la ventana
     email: '',
     password: '',
-    valid: true,
-    loginError: '',
+    firstName: '',
+    lastName: '',
+    valid: true, // Estado de validación del formulario
+    loginError: '', // Mensaje de error de inicio de sesión
     emailRules: [
       v => !!v || 'Email is required',
       v => /.+@.+\..+/.test(v) || 'Email must be valid'
@@ -207,6 +219,7 @@ export default {
   methods: {
     ...mapActions(['login', 'register']),
     async handleLogin() {
+      // maneja el inicio de sesión
       if (this.$refs.form.validate()) {
         try {
           await this.login({ email: this.email, password: this.password });
@@ -219,17 +232,28 @@ export default {
       }
     },
     async handleRegister() {
+      // maneja el registro
       if (this.$refs.form.validate()) {
         try {
           const user = { email: this.email, password: this.password };
           await this.register(user);
           this.step--;
+          this.resetForm();
         } catch (error) {
           console.error('Error al registrar usuario:', error);
         }
       }
     },
+    resetForm() {
+      // rrinicia el formulario y sus campos
+      this.email = '';
+      this.password = '';
+      this.firstName = '';
+      this.lastName = '';
+      this.$refs.form.reset();
+    },
     setAlertTimeout() {
+      // configura el temporizador para ocultar mensajes de error
       if (this.alertTimeout) {
         clearTimeout(this.alertTimeout);
       }
@@ -238,6 +262,7 @@ export default {
       }, 2000);
     },
     clearError() {
+      // limpia el mensaje de error
       this.loginError = '';
       if (this.alertTimeout) {
         clearTimeout(this.alertTimeout);
@@ -245,6 +270,7 @@ export default {
     }
   },
   beforeDestroy() {
+    // limpia el temporizador antes que destruya el componente
     if (this.alertTimeout) {
       clearTimeout(this.alertTimeout);
     }
